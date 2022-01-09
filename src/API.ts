a7a26a02381e350a2b088288452765fa36493534
riansyah <mriansyah93@gmail.com>
2022-01-09 13:17:44 +0800

    init several files

---
+++ src/API.ts
@@ -0,0 +1,6 @@
+export const fetchQuizQuestions = async (
+    amount: number,
+    difficulty: Difficulty
+) => {
+    
+}
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+// Styles
+// import {Wrapper, ButtonWrapper} from './QuestionCard.styles'
+
+type Props = {
+    question: string;
+    answers: string [];
+    callback: any;
+    userAnswer: any;
+    questionNr: number;
+    totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({
+  question, 
+  answers,
+  callback,
+  userAnswer,
+  questionNr, 
+  totalQuestions,  
+}) => (
+    <div> 
+      <p className='number'>
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{__html: question}}></p>
+      {/* parenthes */}
+      {answers.map((answer) => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{__html: answer}}></span>
+          </button>
+        </div>
+      ))}
+    </div>
+)
+export default QuestionCard;
